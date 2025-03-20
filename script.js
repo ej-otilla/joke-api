6341fd458f86d25d1c3306e119bb25a1ef014075
Evan James Otilla <eja.otilla@gmail.com>
2025-03-19 21:59:39 -0600

    Fetch and display joke from JokeAPI with handling for single and two-part jokes on landing screen

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 heading.textContent = "Joke Of The Day 😹"
 joke.textContent = "Loading..."
+
+// Joke Of The Day
+
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    if (data.type === "single") {
+        joke.textContent = data.joke;
+      }
+      else if (data.type === "twopart") {
+        joke.textContent = data.setup + " ... " + data.delivery;
+      }
+});
