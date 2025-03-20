ced0c2fd4bf0a37ee093daf6560f1dea7635f7ca
Evan James Otilla <eja.otilla@gmail.com>
2025-03-19 22:00:55 -0600

    Fetch jokes by category with dynamic headings and emoji on button click

--- script.js
+++ script.js
@@ -15,3 +15,47 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
         joke.textContent = data.setup + " ... " + data.delivery;
       }
 });
+
+// Jokes By Category
+
+function getJoke(category, emoji) {
+  if (category === "Pun") {
+    heading.textContent = `A Random ${category} ${emoji}`;
+  }
+  else {
+    heading.textContent = `A Random ${category} Joke ${emoji}`;
+  }
+
+  fetch(`https://v2.jokeapi.dev/joke/${category.toLowerCase()}?safe-mode`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      if (data.type === "single") {
+        joke.textContent = data.joke;
+      }
+      else if (data.type === "twopart") {
+        joke.textContent = data.setup + " ... " + data.delivery;
+      }
+    })
+}
+
+christmas.addEventListener('click', function() {
+  getJoke("Christmas", "🎅");
+});
+
+programming.addEventListener('click', function() {
+  getJoke("Programming", "💻");
+});
+
+spooky.addEventListener('click', function() {
+  getJoke("Spooky", "👻");
+});
+
+pun.addEventListener('click', function() {
+  getJoke("Pun", "🤭");
+});
+
+misc.addEventListener('click', function() {
+  getJoke("Miscellaneous", "🤔");
+});
